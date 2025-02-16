f7b7f55dc8b27543bd93ceabdbde7952c1937455
PrincessBz <162399259+PrincessBz@users.noreply.github.com>
2025-02-16 00:21:52 -0900

    Added the codes for the todo list

--- index.js
+++ index.js
@@ -1,24 +1,28 @@
-const express = require('express');
-const path = require('path');
-const bodyParser = require('body-parser');
+const express = require("express");
+const path = require("path");
+const bodyParser = require("body-parser");
 
 const port = 3000;
 const app = express();
 const currentTasks = ["Buy Groceries"];
 
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static('public'));
+app.use(express.static("public"));
 
-app.post('/todos', (request, response) => {
-    const newTodo = request.body.todo;
-    if (newTodo) {
-        currentTasks.push(newTodo);
-    }
-    response.redirect('/');
+app.get("/", (request, response) => {
+  response.render("index", { tasks: currentTasks });
+});
+
+app.post("/todos", (request, response) => {
+  const newTodo = request.body.todo;
+  if (newTodo) {
+    currentTasks.push(newTodo);
+  }
+  response.redirect("/");
 });
 
 app.listen(port, () => {
-    console.log(`Server running on http://localhost:${port}`);
+  console.log(`Server running on http://localhost:${port}`);
 });
